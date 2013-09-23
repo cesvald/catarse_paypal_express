06d31e0659b06da12578291c67b803a4dc1964fc
Daniel Weinmann <danielweinmann@gmail.com>
2013-09-23 15:00:07 -0300

    Making default_currency as USD instead of I18n

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,2 +1,2 @@
-ActiveMerchant::Billing::PaypalExpressGateway.default_currency = t('projects.backers.checkout.paypal_currency')
+ActiveMerchant::Billing::PaypalExpressGateway.default_currency = "USD"
 ActiveMerchant::Billing::Base.mode = :test if (PaymentEngines.configuration[:paypal_test] == 'true' rescue nil)
